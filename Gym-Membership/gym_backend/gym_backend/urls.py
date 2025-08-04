15ef63ca1bc4543eed0ca21729c794a301d16229
Chaudharyjayant <chaudharyjayant980@gmail.com>
2025-08-04 11:43:41 +0530

    created an api which will take the information and store it in database and also tested it

--- Gym-Membership/gym_backend/gym_backend/urls.py
+++ Gym-Membership/gym_backend/gym_backend/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from members.views import GymMemberViewSet
+
+router = routers.DefaultRouter()
+router.register(r'members',GymMemberViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- Gym-Membership/gym_backend/members/models.py
+++ Gym-Membership/gym_backend/members/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    
+    GENDER_CHOICES = [
+        ('M','Male'),
+        ('F','Female'),
+    ]
+
+    MEMBERSHIP_CHOICES = [
+       ('Premium'),
+       ('Basic'),
+    ]
+
+    name = models.CharField(max_length=100)
+    age = models.PositiveBigIntegerField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    join_date = models.DateField(auto_now_add=True)
+    membership_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
